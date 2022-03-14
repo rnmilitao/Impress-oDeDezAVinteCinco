25eaa36255594d4cf1adfb2b23e16406b8823027
Rozicleide <roziibr@hotmail.com>
2022-03-14 14:59:58 -0400

    Add project files.

---
+++ ImpressãoDeDezAVinteCinco/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpressãoDeDezAVinteCinco
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 10; i <= 25; i++)
+                Console.WriteLine(i);
+            Console.ReadKey();
+        }
+    }
+}
